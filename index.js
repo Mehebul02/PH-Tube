281126c9c31f1b070873c2a38ed8744e17f40f3e
Mehebul02 <mehebul2123@gmail.com>
2024-03-02 01:16:59 +0600

    total added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const loadBtnContainer = async () => {
+const loadBtnContainer = async (id) => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
   );
@@ -8,21 +8,36 @@ const loadBtnContainer = async () => {
   const btnContainer = document.getElementById("btn-container");
   categories.forEach((item) => {
     const div = document.createElement("div");
-    div.innerHTML = `<button class="btn  btn-ghost bg-slate-700 text-white text-lg">${item.category}</button>`;
+    div.innerHTML = `<button onclick="fetchCategory('${item.category_id}')" class="btn  btn-ghost bg-slate-700 text-white text-lg">${item.category}</button>`;
 
     btnContainer.appendChild(div);
   });
 };
-// load Video 
-const loadVideo = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
-    const data = await res.json();
-     const categoryVideo = data.data;
-     const cardContainer = document.getElementById('card-container');
-     categoryVideo.forEach((item) =>{
-        const div = document.createElement('div');
-        div.classList=`card w-full bg-base-100 shadow-xl`
-        div.innerHTML = ` <figure class="overflow-hidden h-72">
+let selectCategory = 100;
+const fetchCategory = async (categoryId) => {
+  selectCategory = categoryId;
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await res.json();
+  const categoryVideo = data.data;
+
+  const cardContainer = document.getElementById("card-container");
+  const errorEl = document.getElementById("error-element");
+  if (categoryVideo.length === 0) {
+    errorEl.classList.remove("hidden");
+  } else {
+    errorEl.classList.add("hidden");
+  }
+  cardContainer.innerHTML = "";
+  categoryVideo.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList = `card w-full bg-base-100 shadow-xl`;
+    let verifiedBridge = "";
+    if (item.authors[0].verified) {
+      verifiedBridge = `<img class="w-6 h-6" src="./images/verify.png" alt="">`;
+    }
+    div.innerHTML = ` <figure class="overflow-hidden h-72">
         <img class="w-full" src='${item.thumbnail}' alt="Shoes" />
         <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
     </figure>
@@ -35,15 +50,15 @@ const loadVideo = async () =>{
                 <h2 class="card-title">${item.title}</h2>
                 <div class="flex mt-3">
                     <p class="">${item.authors[0].profile_name}</p>
-                    <img class="w-6 h-6" src="./images/verify.png" alt="">
+                   ${verifiedBridge}
                 </div>
                 <p class="mt-3">${item.others.views}</p>
             </div>
         </div>
     </div>`;
-    cardContainer.appendChild(div)
+    cardContainer.appendChild(div);
+  });
+};
 
-     })
-}
-loadVideo()
+fetchCategory();
 loadBtnContainer();
